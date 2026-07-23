2eb21c5c84fcb3fbf6aead53c8d305349e8b4aea
benn kim <whdqja0524@gmail.com>
2026-07-23 11:02:38 +0900

    feat: Cart, CartItem Entity 구현

---
+++ src/main/java/com/lionproject24/fruitshop/entity/Cart.java
@@ -0,0 +1,37 @@
+package com.lionproject24.fruitshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart")
+    @Builder.Default
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
